131edd244fce4bea1145bee19064b193abb1f4db
Tushar <tusharbhatia599@gmail.com>
2023-10-13 00:47:35 +0530

    Initial commit

---
+++ smart-brain-api-main/controllers/image.js
@@ -0,0 +1,57 @@
+const handleApiCall = (req, res, fetch) => {
+  const imageUrl = req.body.input
+  const returnClarifaiRequestOptions = (url) => {
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: "clarifai",
+        app_id: "main",
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: url,
+            },
+          },
+        },
+      ],
+    })
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: process.env.CLARIFAI_API_KEY,
+      },
+      body: raw,
+    }
+    return requestOptions
+  }
+
+  fetch(
+    `https://api.clarifai.com/v2/models/face-detection/outputs`,
+    returnClarifaiRequestOptions(imageUrl)
+  )
+    .then((response) => response.text())
+    .then((data) => {
+      res.json(data)
+    })
+    .catch((err) => res.status(400).json("unable to work with API"))
+}
+
+handleImage = (req, res, db) => {
+  const { id } = req.body
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0].entries)
+    })
+    .catch((err) => res.status(400).json("unable to get count"))
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall,
+}
